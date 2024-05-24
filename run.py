1c69bc033ae0109fb8fe15588e397b66a1a00a3e
Jan R <yanidruffy@gmail.com>
2024-05-24 12:31:15 +0000

    Create main function and ask user to choose a letter. Handle error message, if not alphabetical input and check if user input matches the searched word.

--- run.py
+++ run.py
@@ -23,5 +23,34 @@ animal_list = [
     "Tortoise"
 ]
 
-chosen_word = random.choice(animal_list)
-print(chosen_word)
+def animal_generator():
+    """
+    Return random animal from the list.
+    """
+    return random.choice(animal_list).lower()
+
+def player_check(animal):
+    """
+    A prompt for the player to choose a letter and see if it is entailed within the searched word.
+    """
+    player_choice = input("Choose a letter:").lower()
+    if not player_choice.isalpha():
+        print("Error, try again\n")
+        player_check(animal)
+    else:
+        for letter in animal:
+            if letter == player_choice:
+                print("Correct")
+            else:
+                print("Incorrect, try again.")
+
+
+def main():
+    """
+    Main function to start the game.
+    """
+    print("Welcome Message")
+    animal = animal_generator()
+    print(animal)
+    player_check(animal)
+main()
