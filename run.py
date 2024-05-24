21b18ec6a42e032f065b0a138fbdd9300f790b4f
Jan R <yanidruffy@gmail.com>
2024-05-24 15:00:22 +0000

    Create functions to display placeholders for searched animal and replace placeholders when user guesses a letter correctly

--- run.py
+++ run.py
@@ -31,19 +31,32 @@ def animal_generator():
 
 def player_check(animal):
     """
-    A prompt for the player to choose a letter and see if it is entailed within the searched word.
+    A prompt for the player to choose a letter and check if input is valid.
     """
     player_choice = input("Choose a letter:").lower()
-    if not player_choice.isalpha():
+    if not player_choice.isalpha() or len(player_choice) != 1:
         print("Error, try again\n")
         player_check(animal)
     else:
-        for letter in animal:
-            if letter == player_choice:
-                print("Correct")
-            else:
-                print("Incorrect, try again.")
+        return player_choice
 
+def display_generator(animal):
+    display = []
+
+    for char in animal:
+        display += "_"
+
+    print(display)
+    return display
+
+def update_display(animal, player_choice, display):    
+    for i in range(len(animal)):
+        letter = animal[i]
+
+        if letter == player_choice:
+            display[i] = letter
+
+    print(display)
 
 def main():
     """
@@ -51,6 +64,8 @@ def main():
     """
     print("Welcome Message")
     animal = animal_generator()
+    display = display_generator(animal)
     print(animal)
-    player_check(animal)
+    player_choice = player_check(animal)
+    update_display(animal, player_choice, display)
 main()
